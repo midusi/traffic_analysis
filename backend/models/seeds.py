72bc05b449171e69fd288a132fba8dd86e98e77f
Franco Kumichel <francokumichel1996@gmail.com>
2024-08-25 16:33:25 -0300

    Modifico seeds para agregar mas pruebas

--- backend/models/seeds.py
+++ backend/models/seeds.py
@@ -7,19 +7,14 @@ def run():
 
     print("Cargando datos...")
 
-<<<<<<< HEAD
     prueba_1 = prueba.create_prueba(name="Prueba 1")
     prueba_2 = prueba.create_prueba(name="Prueba 2")
-=======
-     prueba_1 = prueba.create_prueba(name="Prueba 1")
-     prueba_2 = prueba.create_prueba(name="Prueba 2")
-     prueba_3 = prueba.create_prueba(name="Prueba 3")
-     prueba_4 = prueba.create_prueba(name="Prueba 4")
-     prueba_5 = prueba.create_prueba(name="Prueba 5")
-     prueba_6 = prueba.create_prueba(name="Prueba 6")
-     prueba_7 = prueba.create_prueba(name="Prueba 7")
-     prueba_8 = prueba.create_prueba(name="Prueba 8")
->>>>>>> feature/set-up
+    prueba_3 = prueba.create_prueba(name="Prueba 3")
+    prueba_4 = prueba.create_prueba(name="Prueba 4")
+    prueba_5 = prueba.create_prueba(name="Prueba 5")
+    prueba_6 = prueba.create_prueba(name="Prueba 6")
+    prueba_7 = prueba.create_prueba(name="Prueba 7")
+    prueba_8 = prueba.create_prueba(name="Prueba 8")
 
     crear_usuario(
         nombre="Admin",
