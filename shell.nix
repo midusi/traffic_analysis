8d1527d769122a6df2234459529edaf7ea51b00c
tavo <tavo.lop33@gmail.com>
2024-09-20 12:14:29 -0300

    shell version reducida

--- shell.nix
+++ shell.nix
@@ -7,12 +7,5 @@ pkgs.mkShell {
     pkgs.pkg-config
     pkgs.libmysqlclient
     pkgs.python312Packages.setuptools
-    pkgs.python312Packages.wheel
   ];
-
-  shellHook = ''
-    export PKG_CONFIG_PATH=${pkgs.libmysqlclient.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
-    export MYSQLCLIENT_CFLAGS=$(pkg-config --cflags mysqlclient)
-    export MYSQLCLIENT_LDFLAGS=$(pkg-config --libs mysqlclient)
-  '';
 }
